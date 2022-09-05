06eefaafbf754ae33052b3ef017d5acc59ce5c7b
yoonriv <spuerslehd@naver.com>
2022-09-05 21:42:17 +0900

    add main.cc

---
+++ main.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <count.h>
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char*argv[])
+{
+	//int num;
+	//cin >> num;
+	cout << "Hello, world!" << endl;
+	count_(10);
+	return 0;
+					
+}
