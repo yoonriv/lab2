271cdf8157b5f4574601cf911e25095be2920161
yoonriv <spuerslehd@naver.com>
2022-09-05 21:43:28 +0900

    count.cc

---
+++ count.cc
@@ -0,0 +1,14 @@
+#include "count.h"
+#include <iostream>
+using namespace std
+
+void count_(int num)
+{
+		for(int i=1; i<num+1; i++)
+		{
+			cout << i << endl;
+		}
+		cout << "Start!!" << endl;
+		cout << 2017314702 << " " << "윤석준" << endl;
+}
+
